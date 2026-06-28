bc9dcd53be022cb3cd218ff2b0a830414d1809a3
Jaroslaw <jaroslawjaworski95@gmail.com>
2026-06-28 21:21:49 +0000

    Add tabs configuration

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nvim
+## Config
+## Commands
+- `:colo` changes the color scheme, use with <Ctrl-d> to display the list of available color schemes
+- `:Explore` opens NeoVim built-in file explorer. There are other variants like:
+    - `:Sexplore` opens it in a horizontal split, so your file stays visible above/below
+    - `:Vexplore` opens it in a vertical split, side by side with your file
+    - `:Texplore` opens it in a new tab
+    - `:Lexplore` toggles a left-hand sidebar panel (NERDTree-style), and running it again closes it.
--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-vim.opt.termguicolors = true -- Adds full color support
-vim.cmd.colorscheme("catppuccin") -- Sets up color scheme. You can see available color schemes with command ":colo <Ctrl-d>"
 -- ======================================================================
 -- OPTIONS
 -- ======================================================================
@@ -10,4 +8,15 @@ vim.opt.wrap = false -- don't wrap line by default
 vim.opt.cursorline = true -- highlight current line
 vim.opt.scrolloff = 10 -- lock 10 lines above/belove the cursor
 vim.opt.sidescrolloff = 10 -- lock 10 lines left/rigth the cursor
+
 -- Tabs
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2 -- This and 2 settings above set the tab length to 2 basicly
+vim.opt.expandtab = true -- use sapces instead of tabs
+vim.opt.smartindent = true -- enable smart auto-indent
+vim.opt.autoindent = true -- copy indent from the current line
+
+-- Theme
+vim.opt.termguicolors = true -- Adds full color support
+vim.cmd.colorscheme("catppuccin") -- Sets up color scheme. You can see available color schemes with command ":colo <Ctrl-d>"
